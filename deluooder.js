a2dc159cfef98632290b6c08a43b7d5e298b0d5a
Etimo <gitimo@gmail.com>
2023-02-24 18:39:35 +0100

    first functionnal version with API

--- deluooder.js
+++ deluooder.js
@@ -2,37 +2,49 @@ const puppeteer = require('puppeteer');
 const { selectors, languages } = require('./deluoode.json');
 
 class Deluooder {
-  constructor() {
 
+  constructor() {
     this.browser = null;
     this.page = null;
-
-    (async () => {
-
-      //await page.goto('https://translate.google.fr/');
-
-      // Set screen size
-      //await page.setViewport({width: 1920, height: 1080});
-
-      // Accepter CGU :
-      //await page.click(selectors.accept);
-      //await page.waitForNavigation({waitUntil: 'networkidle0'});
-
-    })();
   }
 
+  async init()  {
+
+    this.browser = await puppeteer.launch({headless: false, slowMo: 50});
+    this.page = await this.browser.newPage();
+    
+    await this.page.goto('https://translate.google.fr/');
+
+    // Set screen size
+    await this.page.setViewport({width: 1080, height: 1024});
+
+    // Accepter CGU :
+    await this.page.click(selectors.accept);
+    await this.page.waitForNavigation({waitUntil: 'networkidle0'});
+
+    await this.setLanguageFrom(languages.fr);
+    await this.setSentence('Coucou cest moi');
+    await this.setLanguageTo(languages['zh-tw']);
+    await this.getTranslation().then((res) => {
+      console.log(res);
+    });
+
+  };
+  
   async setLanguageFrom(language) {
     await this.page.click(selectors.sourceLang);
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await this.page.waitForSelector(selectors.searchLang);
     await this.page.keyboard.type(language);
     await this.page.keyboard.press('Enter');
+    await new Promise(resolve => setTimeout(resolve, 1000));
   }
 
   async setLanguageTo(language) {
     await this.page.click(selectors.targetLang);
-    await new Promise(resolve => setTimeout(resolve, 1000));
+    await this.page.waitForSelector(selectors.searchLang);
     await this.page.keyboard.type(language);
     await this.page.keyboard.press('Enter');
+    await new Promise(resolve => setTimeout(resolve, 1000));
   }
 
   async setSentence(text) {
@@ -46,24 +58,15 @@ class Deluooder {
     return value;
   }
 
-  async init() {
-    this.browser = await puppeteer.launch({ headless: false });
-    this.page = await browser.newPage();
-  }
-
   hello_deluoode() {
     console.log("hello deluoode");
   }
 }
 
+
 async function main() {
   const deluooder = new Deluooder();
   await deluooder.init();
-  deluooder.hello_deluoode();
-  deluooder.setLanguageFrom(languages.fr);
-  deluooder.setSentence('Salut je suis un cascadeur professionnel');
-  deluooder.setLanguageTo(languages.en);
-  console.log(deluooder.getTranslation());
 }
 
 if (require.main === module) {
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const deluooder = new (require('./deluooder'));
+const { selectors, languages } = require('./deluoode.json');
 
 const express = require('express');
 const app = express();
@@ -16,16 +17,18 @@ const port = process.env.PORT || '80';
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.post('/translate/*/*', (req, res) => {
+app.post('/translate/*/*', async (req, res) => {
   const languageFrom = req.params[0];
   const languageTo = req.params[1];
   const sentence = req.body.sentence;
 
-  deluooder.setLanguageFrom(languageFrom);
-  deluooder.setLanguageTo(languageTo);
-  deluooder.setSentence(sentence);
+  await deluooder.init();
 
-  const translation = deluooder.getTranslation();
+  await deluooder.setLanguageFrom(languages[languageFrom]);
+  await deluooder.setLanguageTo(languages[languageTo]);
+  await deluooder.setSentence(sentence);
+
+  const translation = await deluooder.getTranslation();
 
   res.send({translation})
 })
