e96b0b675b7a87fbf8527f47477fa5b1f2655129
Etimo <gitimo@gmail.com>
2023-02-24 14:44:23 +0100

    dico json + functions

--- deluooder.js
+++ deluooder.js
@@ -1,56 +1,54 @@
 const puppeteer = require('puppeteer');
-const languages = require('./languages.json');
+const { selectors, languages } = require('./deluoode.json');
 
 class Deluooder {
   constructor() {
-    (async () => {
-      const browser = await puppeteer.launch({ headless: false });
-      const page = await browser.newPage();
 
-      await page.goto('https://translate.google.fr/');
+    this.browser = null;
+    this.page = null;
+
+    (async () => {
+
+      //await page.goto('https://translate.google.fr/');
 
       // Set screen size
-      await page.setViewport({width: 1920, height: 1080});
+      //await page.setViewport({width: 1920, height: 1080});
 
-      await page.mouse.click(1057, 843);
-      await page.waitForNavigation({waitUntil: 'networkidle0'});
-      await page.type('.er8xn', 'Totobono il est trop beau');
+      // Accepter CGU :
+      //await page.click(selectors.accept);
+      //await page.waitForNavigation({waitUntil: 'networkidle0'});
 
-      // Déroule les langues
-      await page.mouse.click(1293, 156);
-      
-      await new Promise(resolve => setTimeout(resolve, 1000));
-      await page.keyboard.type('Anglais');
-      await page.keyboard.press('Enter');
-
-      //let selectorLanguage = 'div[data-language-code="en"]';
-
-      //await page.waitForSelector(selectorLanguage);
-      //let slBox = await (await page.$(selectorLanguage)).boundingBox();
-      //await page.mouse.click(slBox.x, slBox.y);
-
-      //console.log(slBox);
-
-      await page.waitForSelector('.ryNqvb', {delay: 5000});
-      let element = await page.$('.ryNqvb');
-      let value = await page.evaluate(el => el.textContent, element);
-      console.log(value);
-
-    
     })();
   }
 
-  setLanguageFrom(language) {
+  async setLanguageFrom(language) {
+    await this.page.click(selectors.sourceLang);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await this.page.keyboard.type(language);
+    await this.page.keyboard.press('Enter');
   }
 
-  setLanguageTo(language) {
+  async setLanguageTo(language) {
+    await this.page.click(selectors.targetLang);
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await this.page.keyboard.type(language);
+    await this.page.keyboard.press('Enter');
   }
 
-  setSentence(text) {
+  async setSentence(text) {
+    await this.page.type(selectors.input, text);
   }
 
-  getTranslation() {
-    return ""
+  async getTranslation() {
+    await this.page.waitForSelector(selectors.output);
+    let element = await this.page.$(selectors.output);
+    let value = await this.page.evaluate(el => el.textContent, element);
+    return value;
+  }
+
+  async init() {
+    this.browser = await puppeteer.launch({ headless: false });
+    this.page = await browser.newPage();
   }
 
   hello_deluoode() {
@@ -58,9 +56,14 @@ class Deluooder {
   }
 }
 
-function main() {
+async function main() {
   const deluooder = new Deluooder();
+  await deluooder.init();
   deluooder.hello_deluoode();
+  deluooder.setLanguageFrom(languages.fr);
+  deluooder.setSentence('Salut je suis un cascadeur professionnel');
+  deluooder.setLanguageTo(languages.en);
+  console.log(deluooder.getTranslation());
 }
 
 if (require.main === module) {
